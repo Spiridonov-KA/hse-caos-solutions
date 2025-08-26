9180d5a95d458d6d5467ec7eb5f17d4bec0f6351
Oleg Shatov <oleq.shatov@yandex.ru>
2025-08-26 19:35:46 +0000

    Fix public export

--- common/tools/export_to.sh
+++ common/tools/export_to.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ $# != 1 ]]; then
-    echo "Usage: $0 <public_dir>"
+if (( $# < 1 )); then
+    echo "Usage: $0 <public_dir> [extra-args ...]"
     exit 1
 fi
 
-cargo xtask compose --in-path . --out-path $1 --skip .git --skip target --skip .cargo-home --stat
+public_dir=$1
+shift
+
+cargo xtask compose \
+    --in-path . \
+    --out-path $public_dir \
+    --skip .git \
+    --skip target \
+    --skip .cargo-home \
+    --stat \
+    $@
