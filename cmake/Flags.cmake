16778279a6cf047bc7f608760040ac0f06919f8a
Oleg Shatov <oleq.shatov@yandex.ru>
2025-09-20 15:17:41 +0000

    Fix false-positive warnings

--- cmake/Flags.cmake
+++ cmake/Flags.cmake
@@ -1,3 +1,5 @@
+include(CheckCXXCompilerFlag)
+
 set(CMAKE_CXX_FLAGS_ASAN
     "${CMAKE_CXX_FLAGS_ASAN} -g -fsanitize=address,undefined -fno-sanitize-recover=all -DASAN"
     CACHE STRING "Additional compiler flags in asan builds"
@@ -25,3 +27,13 @@ set(CMAKE_CXX_FLAGS_RELEASE
 set(CMAKE_CXX_FLAGS
     "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror"
 )
+
+function (add_flag_if_supported flag)
+    check_cxx_compiler_flag(flag HAS_FLAG)
+    if (HAS_FLAG)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+    endif()
+endfunction()
+
+add_flag_if_supported("-Wno-array-bounds")
+add_flag_if_supported("-Wno-stringop-overread")
