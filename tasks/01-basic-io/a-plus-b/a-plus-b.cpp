b913fa7fd4244ad19d701cccf5d9171b6253add2
Kirirll Spiridonov <vo-ro@list.ru>
2025-12-21 01:15:20 +0300

    solve 2 tasks in first hw

--- tasks/01-basic-io/a-plus-b/a-plus-b.cpp
+++ tasks/01-basic-io/a-plus-b/a-plus-b.cpp
@@ -1,9 +1,5 @@
 #include "a-plus-b.h"
 
-#include <unused.hpp>  // TODO: remove before flight.
-
 uint32_t Sum(uint32_t a, uint32_t b) {
-    UNUSED(a, b);  // TODO: remove before flight.
-    // TODO: your code here.
-    throw "TODO";
+    return a + b;
 }
--- tasks/01-basic-io/char-sum/solution.cpp
+++ tasks/01-basic-io/char-sum/solution.cpp
@@ -1,6 +1,61 @@
+#include <algorithm>
 #include <cctype>  // std::isdigit
+#include <charconv>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fcntl.h>
+#include <string>
+#include <sys/types.h>
+#include <system_error>
 #include <unistd.h>
+#include <vector>
+
+std::vector<char> readStdin() {
+    int numBytes;
+    char buf[BUFSIZ];
+    std::vector<char> input;
+    input.reserve(BUFSIZ);
+    while ((numBytes = read(STDIN_FILENO, buf, BUFSIZ)) > 0) {
+        input.insert(input.end(), buf, buf + numBytes);
+    }
+    return input;
+}
+
+int sumDigitsInString(const std::vector<char>& input) {
+    int result = 0;
+    std::for_each(input.begin(), input.end(), [&result](char x) {
+        if (std::isdigit(x)) {
+            result += x - '0';
+        }
+    });
+    return result;
+}
+
+void writeStdout(const std::string& str) {
+    auto totalBytes = str.size();
+    ssize_t numBytes = 0;
+    size_t writtenBytes = 0;
+    while ((numBytes = write(STDOUT_FILENO, str.c_str() + writtenBytes,
+                             totalBytes - writtenBytes)) > 0) {
+        writtenBytes += numBytes;
+    }
+    if (write(1, "\n", 1) < 0) {
+        exit(1);
+    }
+}
 
 int main() {
-    // TODO: your code here.
+    std::vector<char> input = readStdin();
+    int result = sumDigitsInString(input);
+
+    char buf[BUFSIZ];
+    auto [_, ec] = std::to_chars(buf, buf + BUFSIZ, result);
+
+    if (ec != std::errc{}) {
+        writeStdout(std::make_error_code(ec).message());
+    } else {
+        writeStdout(buf);
+    }
 }
