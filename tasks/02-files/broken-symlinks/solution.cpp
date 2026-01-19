684a6d8a63405f7c82dfa70e26908ae4d4625f95
Kirirll Spiridonov <vo-ro@list.ru>
2026-01-19 12:17:35 +0300

    solved "broken-symlinks" problem

--- tasks/02-files/broken-symlinks/solution.cpp
+++ tasks/02-files/broken-symlinks/solution.cpp
@@ -1,12 +1,31 @@
+#include <cerrno>
 #include <iostream>
 
+#include <cstring>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <unused.hpp>  // TODO: remove before flight.
+void PrintFileInfo(const char* filename) {
+    struct stat sb;
+    if (stat(filename, &sb) != 0) {
+        if (lstat(filename, &sb) == 0) {
+            std::cout << filename << " (broken symlink)" << std::endl;
+            return;
+        }
+        if (errno == ENOENT) {
+            std::cout << filename << " (missing)" << std::endl;
+        } else {
+            std::cout << std::strerror(errno) << " WTF!\n";
+        }
+
+    } else {
+        std::cout << filename << '\n';
+    }
+}
 
 int main(int argc, const char* argv[]) {
-    UNUSED(argc, argv);  // TODO: remove before flight.
-    // TODO: your code here.
+    for (int i = 1; i < argc; ++i) {
+        PrintFileInfo(argv[i]);
+    }
 }
