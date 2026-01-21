bee1248b3346e06a52cf20a2d1f1d55c8b0ffa0f
Kirirll Spiridonov <vo-ro@list.ru>
2026-01-21 19:56:12 +0300

    solve "buf-writer" problem

--- tasks/02-files/buf-writer/writer.hpp
+++ tasks/02-files/buf-writer/writer.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <cassert>
+#include <cerrno>
+#include <cstddef>
+#include <cstring>
+#include <iostream>
 #include <string_view>
 #include <unistd.h>
 
 #include <unused.hpp>  // TODO: remove before flight.
 
 struct BufWriter {
-    BufWriter(int fd, size_t buf_capacity) : fd_(fd) {
-        UNUSED(buf_capacity);  // TODO: remove before flight.
+    BufWriter(int fd, size_t buf_capacity)
+        : fd_{fd}, size_{0}, capacity_{buf_capacity} {
+        buffer_ = new char[capacity_];
     }
 
     // Non-copyable
@@ -19,23 +25,59 @@ struct BufWriter {
     BufWriter& operator=(BufWriter&&) = delete;
 
     int Write(std::string_view data) {
-        UNUSED(data, fd_);  // TODO: remove before flight.
-        // TODO: your code here.
+        if (size_ + data.size() <= capacity_) {
+            std::memcpy(buffer_ + size_, data.data(), data.size());
+            size_ += data.size();
+            return 0;
+        }
+        if (int res = Flush(); res != 0) {
+            return res;
+        }
+
+        if (data.size() > capacity_) {
+            size_t tail_size = data.size() % capacity_;
+            size_t big_chunk_size = data.size() - tail_size;
+
+            if (int res = FlushArray(data.data(), big_chunk_size); res != 0) {
+                return res;
+            }
+
+            std::memcpy(buffer_, data.data() + big_chunk_size, tail_size);
+            size_ = tail_size;
+        } else {
+            std::memcpy(buffer_, data.data(), data.size());
+            size_ = data.size();
+        }
+
         return 0;
     }
 
     int Flush() {
-        // TODO: your code here.
-        return 0;
+        int res = FlushArray(buffer_, size_);
+        size_ = 0;
+        return res;
     }
 
     ~BufWriter() {
-        // Best effort
         Flush();
+        delete[] buffer_;
     }
 
   private:
+    int FlushArray(const char* buf, size_t count) {
+        size_t written = 0;
+        while (written < count) {
+            ssize_t res = ::write(fd_, buf + written, count - written);
+            if (res == -1) {
+                return -errno;
+            }
+            written += static_cast<size_t>(res);
+        }
+        return 0;
+    }
 
     int fd_;
-    // TODO: your code here.
+    char* buffer_;
+    size_t size_;
+    size_t capacity_;
 };
